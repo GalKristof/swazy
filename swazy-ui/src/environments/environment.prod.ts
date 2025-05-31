e9a3bfcb783707a0f4b700c92d3c9b4c92ca7a94
Gál Kristóf <gal.kristof08@gmail.com>
2025-05-31 20:45:45 +0200

    basics for ui

---
+++ swazy-ui/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+﻿export const environment = {
+  production: true,
+  apiUrl: 'https://swazy-dev.hu/api',
+  syncfusionLicense: 'Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXpdcHVSQmNeU0N+WEVWYUA=',
+};
