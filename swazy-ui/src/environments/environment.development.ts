2d62d66f7a93779245ac294e1783ee6ca4e0e6da
Gál Kristóf <gal.kristof08@gmail.com>
2025-09-30 19:45:25 +0200

    add small fixes

--- swazy-ui/src/environments/environment.development.ts
+++ swazy-ui/src/environments/environment.development.ts
@@ -2,5 +2,6 @@
   production: false,
   apiUrl: 'https://localhost:7283/api',
   fromDomain: false,
-  tenantId: '2eaf53c8-faa4-4404-b948-7a27124d7c12'
+  //tenantId: '2eaf53c8-faa4-4404-b948-7a27124d7c12' laptop docker
+  tenantId: '3e0ead55-d445-43b0-8adc-d360ca4e8328' // pc docker
 };
